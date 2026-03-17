94a00484ed190f033ad430463356fdd8ab0edbc3
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-03-17 18:52:44 +0100

    good start!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, Dima!");
+        int choice = 2;
+
+
+
+
+        switch (choice) {
+            case 1:
+                System.out.println("You ordered Coffee");
+                break;
+            case 2:
+                System.out.println("You ordered Tea");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
     }
 }
